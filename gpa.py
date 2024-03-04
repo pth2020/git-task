83ea857ef9516c89575b304f87f2b60568e730e5
Paulos Tesfazghi <71388869+pth2020@users.noreply.github.com>
2024-03-04 21:02:03 +0000

    Create gpa.py

---
+++ gpa.py
@@ -0,0 +1,30 @@
+from tabulate import tabulate
+
+class Student:
+
+    def __init__(self,name,grades):
+        self.name = name
+        self.grades = grades
+        
+    def get_average(self):
+        return round(sum(self.grades)/len(self.grades),1)
+    
+student1 = Student("Peter",[80,70,90])
+student2 = Student("Emily",[90,88,100])
+student3 = Student("Simon",[96,82,90])
+student4 = Student("Toby",[45,50,90])
+student5 = Student("Sasha",[100,98,90])
+
+data = []
+
+data.append([student1.name,student1.grades[0],student1.grades[1],student1.grades[2],student1.get_average()])
+data.append([student2.name,student2.grades[0],student2.grades[1],student2.grades[2],student2.get_average()])
+data.append([student3.name,student3.grades[0],student3.grades[1],student3.grades[2],student3.get_average()])
+data.append([student4.name,student4.grades[0],student4.grades[1],student4.grades[2],student4.get_average()])
+data.append([student5.name,student5.grades[0],student5.grades[1],student5.grades[2],student5.get_average()])
+
+headers = ["Name","English","Maths","Science","Average"]
+
+table1 = tabulate(data,headers = headers)
+
+print(table1)
