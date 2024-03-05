66d2ba416064c152bf3243a5c4074edac4d638a0
Paulos Tesfazghi <paultesfa@gmail.com>
2024-03-05 09:03:02 +0000

    Change getAverage func from camelCase to snake_case

--- gpa.py
+++ gpa.py
@@ -6,11 +6,11 @@ class Student:
         self.name = name
         self.grades = grades
         
-    def getAverage(self):
+    def get_average(self):
         return round(sum(self.grades)/len(self.grades),1)
     
     def get_award(self):
-        avg = self.getAverage()
+        avg = self.get_average()
                 
         if avg >= 90:
             return "Distinction"
@@ -29,11 +29,11 @@ student5 = Student("Sasha",[100,98,90])
 
 data = []
 
-data.append([student1.name,student1.grades[0],student1.grades[1],student1.grades[2],student1.getAverage(),student1.get_award()])
-data.append([student2.name,student2.grades[0],student2.grades[1],student2.grades[2],student2.getAverage(),student2.get_award()])
-data.append([student3.name,student3.grades[0],student3.grades[1],student3.grades[2],student3.getAverage(),student3.get_award()])
-data.append([student4.name,student4.grades[0],student4.grades[1],student4.grades[2],student4.getAverage(),student4.get_award()])
-data.append([student5.name,student5.grades[0],student5.grades[1],student5.grades[2],student5.getAverage(),student5.get_award()])
+data.append([student1.name,student1.grades[0],student1.grades[1],student1.grades[2],student1.get_average(),student1.get_award()])
+data.append([student2.name,student2.grades[0],student2.grades[1],student2.grades[2],student2.get_average(),student2.get_award()])
+data.append([student3.name,student3.grades[0],student3.grades[1],student3.grades[2],student3.get_average(),student3.get_award()])
+data.append([student4.name,student4.grades[0],student4.grades[1],student4.grades[2],student4.get_average(),student4.get_award()])
+data.append([student5.name,student5.grades[0],student5.grades[1],student5.grades[2],student5.get_average(),student5.get_award()])
 
 headers = ["Name","English","Maths","Science","Average","Award"]
 
